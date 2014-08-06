affd8c6b2aa389d4dcee1d1fed145501e6a73159
kinging123 <rubik@karasik.org>
2014-08-06 15:06:53 +0300

    removed callbacks

--- js/imiklat.js
+++ js/imiklat.js
@@ -21,7 +21,7 @@ function setMap() {
 function centerMap (latitude, longitude, Markers) {
     // alert("hello");
     // alert(position);
-    alert("the function was called");
+    // alert("the function was called");
     $("#map").goMap({
         latitude: 32.0853,
         longitude: 34.781768,
@@ -41,9 +41,9 @@ function centerMap (latitude, longitude, Markers) {
         icon: 'img/imiklat_sm.png?v=2' 
 
     });
-    alert("The map has been registered.");
+    // alert("The map has been registered.");
     $.goMap.fitBounds(); 
-    alert("The map had been fitBounded.");
+    // alert("The map had been fitBounded.");
     
 }
 position = {
@@ -58,13 +58,13 @@ var json_locs, Markers = new Array,
     defaultNbrhd = "בקעה";
 
 $(function(){
-    alert("Page loaded");
+    // alert("Page loaded");
     $("#send").click(function(event) {
-        alert("#send button was clicked.");
+        // alert("#send button was clicked.");
         cityPass = $("select#city").val();
-        alert("city was readden from inputs.");
+        // alert("city was readden from inputs.");
         nbrhdPass = $("select#nbrhd").val();
-        alert("nbrhd was readden from inputs.");
+        // alert("nbrhd was readden from inputs.");
 
         if(cityPass == otherText)cityPass = $("#inputforcity").val();
         if(nbrhdPass == otherText)nbrhdPass = $("#inputfornbrhd").val();
@@ -73,7 +73,7 @@ $(function(){
         if(cityPass.length < 3)cityPass=defaultCity;
         if(nbrhdPass.length < 3)nbrhdPass=defaultNbrhd;
 
-        alert("starting AJAX.");
+        // alert("starting AJAX.");
         $.ajax({
             url: "http://projects.karasik.org/imiklat/functions/getLocations.php",
             type: "get",
@@ -81,25 +81,25 @@ $(function(){
             data: {"city":cityPass, "nbrhd": nbrhdPass},
             crossDomain: true,
             complete: function(json_locs, status){
-                alert("AJAX returned");
+                // alert("AJAX returned");
                 console.log(json_locs.responseJSON);
-                alert("status: \n" +status);
+                // alert("status: \n" +status);
                 json_locs = json_locs.responseJSON;
                 
                 json_locs = $.parseJSON(json_locs);
 
-                alert("json_locs was parsed, here's a street: " +json_locs[0]["street"]);
+                // alert("json_locs was parsed, here's a street: " +json_locs[0]["street"]);
                 $.each(json_locs, function(locindex, location){
-                    alert("a location is being readden.");
+                    // alert("a location is being readden.");
                     AddressTexttm = location.street + " " + location.house + ", " + location.city;
                     tempAdress = {address: AddressTexttm, title: AddressTexttm};
                     Markers.push(tempAdress);
-                    alert("a location has been added to markers array.");
+                    // alert("a location has been added to markers array.");
                 });
 
-                alert("The map is about to be set.");
+                // alert("The map is about to be set.");
                 centerMap(11, 11, Markers);
-                alert("The map has been set and the function is complete.");
+                // alert("The map has been set and the function is complete.");
                 var markers = [];
 
 
@@ -109,13 +109,13 @@ $(function(){
                         markers.push(temp);
                     }
 
-                    alert("The markers array was generated for the MarkerClusterer.");
+                    // alert("The markers array was generated for the MarkerClusterer.");
                     var markerclusterer = new MarkerClusterer($.goMap.map, markers);
-                    alert("the markerclusterer was registered.");
+                    // alert("the markerclusterer was registered.");
 
                 }, 5000);
 
-                alert("done.");
+                // alert("done.");
             }
         // alert("AJAX was sent.");
         });
